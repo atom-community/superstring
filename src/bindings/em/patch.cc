e60f8c21e3bb75891f119e88903ab186586bfb58
aminya <aminyahyaabadi74@gmail.com>
2020-10-31 02:27:38 -0500

    make it explicit that emscripten::internal is used
    If emscripten removes these API (since they are internal), this code will break

--- src/bindings/em/patch.cc
+++ src/bindings/em/patch.cc
@@ -11,18 +11,14 @@ using std::vector;
 
 template <>
 inline Patch const *emscripten::val::as<Patch const *>(void) const {
-  using namespace emscripten;
-  using namespace internal;
-
-  EM_DESTRUCTORS destructors;
-  EM_GENERIC_WIRE_TYPE result = _emval_as(
-    handle,
-    TypeID<AllowedRawPointer<Patch const>>::get(),
+  emscripten::internal::EM_DESTRUCTORS destructors;
+  emscripten::internal::EM_GENERIC_WIRE_TYPE result = emscripten::internal::_emval_as(
+      handle, emscripten::internal::TypeID<emscripten::internal::AllowedRawPointer<Patch const>>::get(),
     &destructors
   );
-  DestructorsRunner destructors_runner(destructors);
+  emscripten::internal::DestructorsRunner destructors_runner(destructors);
 
-  return fromGenericWireType<Patch *>(result);
+  return emscripten::internal::fromGenericWireType<Patch *>(result);
 }
 
 Patch *constructor(emscripten::val value) {
